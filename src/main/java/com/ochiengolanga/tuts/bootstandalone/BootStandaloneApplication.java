c201ddcd149650f73e65a65a748a212b916b36cf
Ochieng Olanga <ochiengolanga@gmail.com>
2018-03-24 17:07:23 +0300

    Initial commit

---
+++ src/main/java/com/ochiengolanga/tuts/bootstandalone/BootStandaloneApplication.java
@@ -0,0 +1,12 @@
+package com.ochiengolanga.tuts.bootstandalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStandaloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootStandaloneApplication.class, args);
+	}
+}
